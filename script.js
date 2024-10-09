b2dfe0e8fda0e8db7e74b4823359b795fb5ab1a5
Kauan Gabriel <kauangabrielfrancisco14@gmail.com>
2024-10-09 10:37:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const btnCloud = document.getElementById("btnCloud");
+const btnSaas = document.getElementById("btnSaas");
+
+btnCloud.addEventListener("click", function(){
+    window.location.href ="index.html";
+})
+
+btnSaas.addEventListener("click", function(){
+    window.location.href ="Saas.html";
+})
